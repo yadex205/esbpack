d636da522adf7055d2ff11b97ee3e785cd874754
Kanon Kakuno <yadex205@outlook.jp>
2021-09-14 15:06:39 +0900

    :recycle: Improve generating d.ts files

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,11 +1,13 @@
+import { exec as _exec } from 'child_process';
 import path from 'path';
+import { promisify } from 'util';
 
 import * as esbuild from 'esbuild';
-import typescript from 'typescript';
 
-import tsconfigJson from '../tsconfig.json';
+const exec = promisify(_exec);
 
 const PKG_DIR = path.join(__dirname, '../packages');
+const TSC_PATH = path.join(__dirname, '../node_modules/.bin/tsc');
 
 const externalizeNodeModulesPlugin: esbuild.Plugin = {
   name: 'externalize-node-modules',
@@ -30,34 +32,8 @@ const externalizeNodeModulesPlugin: esbuild.Plugin = {
   },
 };
 
-// Based on https://github.com/microsoft/TypeScript/wiki/Using-the-Compiler-API
-const buildDts = (filePaths: string[], outDir: string) => {
-  const program = typescript.createProgram(filePaths, {
-    ...tsconfigJson.compilerOptions,
-    module: typescript.ModuleKind.CommonJS,
-    target: typescript.ScriptTarget.ES2019,
-    lib: ['lib.es2019.d.ts'],
-    noEmit: false,
-    declaration: true,
-    emitDeclarationOnly: true,
-    outDir,
-  });
-  const emitResult = program.emit();
-
-  const allDiagnostics = typescript.getPreEmitDiagnostics(program).concat(emitResult.diagnostics);
-  allDiagnostics.forEach(diagnostic => {
-    if (diagnostic.file) {
-      const { line, character } = typescript.getLineAndCharacterOfPosition(diagnostic.file, diagnostic.start!);
-      const message = typescript.flattenDiagnosticMessageText(diagnostic.messageText, '\n');
-      console.warn(`${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`);
-    } else {
-      console.warn(`${typescript.flattenDiagnosticMessageText(diagnostic.messageText, '\n')}`);
-    }
-  });
-
-  if (emitResult.emitSkipped) {
-    throw new Error(`Failed export d.ts to ${outDir}`);
-  }
+const buildTsProject = async (tsconfigJsonPath: string) => {
+  return await exec(`${TSC_PATH} -p ${tsconfigJsonPath}`);
 };
 
 esbuild.build({
@@ -69,17 +45,6 @@ esbuild.build({
   minify: true,
   platform: 'browser',
 });
-
-buildDts(
-  [
-    'assets-manager.ts',
-    'dev-server.ts',
-    'esbuild-plugin-esbpack-internal-assets-manager.ts',
-    'index.ts',
-    'logger.ts',
-  ].map(basename => path.join(PKG_DIR, 'esbpack/src', basename)),
-  path.join(PKG_DIR, 'esbpack/dist')
-);
 
 esbuild.build({
   entryPoints: [path.join(PKG_DIR, 'esbpack/src/index.ts')],
@@ -94,3 +59,6 @@ esbuild.build({
 
   plugins: [externalizeNodeModulesPlugin],
 });
+(async () => {
+  await buildTsProject(path.join(PKG_DIR, 'esbpack/tsconfig.dts.json'));
+})();
